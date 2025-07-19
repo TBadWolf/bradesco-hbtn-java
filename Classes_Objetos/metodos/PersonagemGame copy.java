173dd019cc37c0a0c1a0f7b511159988fdc23be8
TBadWolf <barbara.rce@hotmail.com>
2025-07-19 20:14:31 -0300

    add metodos

---
+++ Classes_Objetos/metodos/PersonagemGame copy.java
@@ -0,0 +1,34 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome; 
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void tomarDano(int quantidadeDeDano){
+        if(saudeAtual > 0){
+        saudeAtual = saudeAtual - quantidadeDeDano;
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura){
+        if(saudeAtual < 100){
+        saudeAtual = saudeAtual + quantidadeDeCura;
+        }
+    }
+}
+
+
