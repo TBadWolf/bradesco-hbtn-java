aea634451309948d1b5a01ae1715284628f181f9
TBadWolf <barbara.rce@hotmail.com>
2025-08-04 10:47:58 -0300

    add interfaces/workflow

---
+++ Interfaces/workflow/ProcessadorVideo.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+public class ProcessadorVideo {
+    private List<CanalNotificacao> canais = new ArrayList<>();
+
+    public void registrarCanal (CanalNotificacao canal) {
+        canais.add(canal);
+    }
+ 
+    public void processar(Video video) {
+        Mensagem mensagem = new Mensagem();
+        mensagem.setTexto(video.getArquivo() + " - " + video.getFormato());
+        mensagem.setTipoMensagem(TipoMensagem.LOG);
+
+        for (CanalNotificacao canal : canais) {
+            canal.notificar(mensagem);
+        }
+    }
+}
