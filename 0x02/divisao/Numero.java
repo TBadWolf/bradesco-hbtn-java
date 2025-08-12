7238d2efa4ff5717c971bfae4f2e0822d6b1716f
TBadWolf <barbara.rce@hotmail.com>
2025-08-12 19:38:38 -0300

    ajuste mensagem exception

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -1,11 +1,11 @@
 public class Numero {
-    
+
     public static void dividir(int a, int b){
         int resultado = 0;
         try{
             resultado = a / b;
         } catch (ArithmeticException e) {
-            System.out.println("Erro: Divisão por zero não é permitida.");
+            System.out.println("Nao eh possivel dividir por zero");
         } catch (Exception e) {
             System.out.println("Erro inesperado: " + e.getMessage());
         }finally {
