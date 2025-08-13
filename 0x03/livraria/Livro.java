b0a686b06aafd9011533dd323a6818e3845f8b46
TBadWolf <barbara.rce@hotmail.com>
2025-08-12 21:24:42 -0300

    add 0x03 livraria

---
+++ 0x03/livraria/Livro.java
@@ -0,0 +1,48 @@
+import exceptions.AutorInvalidoException;
+import exceptions.LivroInvalidoException;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private double preco;
+
+    public Livro(String titulo, String autor, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    public void setTitulo(String titulo) {
+        if(titulo.length() < 3) {
+            throw new LivroInvalidoException("Titulo de livro invalido");
+        }
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        if(autor.length() < 3) {
+            throw new AutorInvalidoException("Nome de autor invalido");
+        }
+        this.autor = autor;
+    }
+
+    public void setPreco(double preco) {
+        if(preco < 0) {
+            throw new LivroInvalidoException("Preco de livro invalido");
+        }
+        this.preco = preco;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+}
