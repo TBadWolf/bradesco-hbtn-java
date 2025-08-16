a1c380108afbff7e3576bf9da8d700c94b2fbc78
TBadWolf <barbara.rce@hotmail.com>
2025-08-16 13:11:21 -0300

    add estrutura de dados/1

---
+++ estruturas_de_dados/1/GestaoAlunos.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class GestaoAlunos {
+    private ArrayList<Aluno> alunos;
+
+    public GestaoAlunos() {
+        this.alunos = new ArrayList<>();
+    }
+
+    public void adicionarAluno(Aluno aluno) {
+        this.alunos.add(aluno);
+    }
+
+    public void removerAluno(Aluno aluno) {
+        Aluno alunoEncontrado = buscarAlunoPorNome(aluno.getNome());
+        if (alunoEncontrado != null) {
+            this.alunos.remove(aluno);
+            return;
+        }
+    }
+
+    public void listarAlunos() {
+        for (Aluno aluno : alunos) {
+            System.out.println(aluno);
+        }
+    }
+
+    public Aluno buscarAlunoPorNome(String nome) {
+        for (Aluno aluno : alunos) {
+            if (aluno.getNome().equalsIgnoreCase(nome)) {
+                return aluno;
+            }
+        }
+        return null;
+    }
+}
