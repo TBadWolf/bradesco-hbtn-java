6e7d78939013f4812339bef67fc998d748b42fe6
TBadWolf <barbara.rce@hotmail.com>
2025-08-16 14:50:52 -0300

    add Generics/ Midia

---
+++ Generics/midias/Livro.java
@@ -0,0 +1,17 @@
+public class Livro extends Midia {
+    private int edicao;
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
+
+    public Livro(String nome, int edicao) {
+        super(nome);
+        this.edicao = edicao;
+    }
+    
+}
