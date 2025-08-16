f78c7bf9b2ceaa455ef9919dad4e5e844cb07f27
TBadWolf <barbara.rce@hotmail.com>
2025-08-16 12:03:25 -0300

    add arquivos/0

---
+++ arquivos/0/FileWritingExercise.java
@@ -0,0 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileWritingExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do arquivo (com extensão .txt): ");
+        String fileName = scanner.nextLine();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            System.out.println("Digite o conteúdo a ser escrito no arquivo('sair' para finalizar):");
+
+            while (true) {
+                String line = scanner.nextLine();
+                if (line.equalsIgnoreCase("sair")) {
+                    break;
+                }
+                writer.write(line);
+                writer.newLine(); // adiciona quebra de linha
+            }
+
+            System.out.println("Arquivo criado com sucesso: " + fileName);
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao escrever no arquivo: " + e.getMessage());
+        }
+        scanner.close();
+    }
+}
