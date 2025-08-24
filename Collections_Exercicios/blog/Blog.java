42fa5278899dd4781155b88c727e7dd64e6489b3
TBadWolf <barbara.rce@hotmail.com>
2025-08-24 20:29:59 -0300

    subindo novo repo

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Blog {
+    private List<Post> postagens;
+
+    public Blog() {
+        this.postagens = new java.util.ArrayList<>();
+    }
+
+    public void adicionarPostagem(Post post) {
+        this.postagens.add(post);
+    }
+
+    public Set<String> obterTodosAutores() {
+        Set<String> autores = new java.util.HashSet<>();
+        for (Post post : postagens) {
+            autores.add(post.getAutor());
+        }
+        return autores;
+    }
+
+    public java.util.Map<String, Integer> obterContagemPorCategoria() {
+        java.util.Map<String, Integer> contagem = new java.util.HashMap<>();
+        for (Post post : postagens) {
+            String categoria = post.getCategoria();
+            contagem.put(categoria, contagem.getOrDefault(categoria, 0) + 1);
+        }
+        return contagem;
+    }
+}
