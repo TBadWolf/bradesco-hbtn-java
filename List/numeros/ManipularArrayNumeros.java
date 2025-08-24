d08ee95562baf224d1c7cb4e10ff6daa0844c073
TBadWolf <barbara.rce@hotmail.com>
2025-08-24 19:47:44 -0300

    add List/numeros

---
+++ List/numeros/ManipularArrayNumeros.java
@@ -0,0 +1,35 @@
+package List.numeros;
+
+import java.util.List;
+
+public class ManipularArrayNumeros {
+
+    public static int buscarPosicaoNumero(List<Integer> lista, int numero) {
+        return lista.indexOf(numero);
+    }
+
+    public static void adicionarNumero(List<Integer> lista, int numero) throws Exception {
+        if (buscarPosicaoNumero(lista, numero) != -1) {
+            throw new Exception("Numero jah contido na lista");
+        }
+        lista.add(numero);
+    }
+
+    public static void removerNumero(List<Integer> lista, int numero) throws Exception {
+        int posicao = buscarPosicaoNumero(lista, numero);
+        if (posicao == -1) {
+            throw new Exception("Numero nao encontrado na lista");
+        }
+        lista.remove(Integer.valueOf(numero));
+    }
+
+    public static void substituirNumero(List<Integer> lista, int numeroSubstituir , int numeroSubstituto) {
+        int posicao = buscarPosicaoNumero(lista, numeroSubstituir);
+        if (posicao != -1) {
+            lista.set(posicao, numeroSubstituto);
+        } else {
+            lista.add(numeroSubstituto);
+        }
+    }
+
+}
