4a99ffa29567b7386ae9a9d95dbddee1414abb5e
TBadWolf <barbara.rce@hotmail.com>
2025-08-24 20:16:18 -0300

    add espaços

--- List/todo/ListaTodo.java
+++ List/todo/ListaTodo.java
@@ -51,7 +51,8 @@ public class ListaTodo {
     public void listarTarefas() {
         for (Tarefa tarefa : tarefas) {
             String status = tarefa.isEstahFeita() ? "[X]" : "[ ]";
-            System.out.println(status + " Id: " + tarefa.getIdentificador() + " - Descricao: " + tarefa.getDescricao());
+            System.out
+                    .println(status + "  Id: " + tarefa.getIdentificador() + " - Descricao: " + tarefa.getDescricao());
         }
     }
 
