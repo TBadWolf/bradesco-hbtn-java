5002297aac83923d242a6f54fbeeab466c0f5620
TBadWolf <barbara.rce@hotmail.com>
2025-09-12 13:54:19 -0300

    correção nome do método

--- Streams_Collectors_Optional/livros/Consulta.java
+++ Streams_Collectors_Optional/livros/Consulta.java
@@ -2,7 +2,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Consulta {
-    public static List<Produto> obterLivrosdoPedido(Pedido pedido) {
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
         return pedido.getProdutos().stream()
                 .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
                 .collect(Collectors.toList());
