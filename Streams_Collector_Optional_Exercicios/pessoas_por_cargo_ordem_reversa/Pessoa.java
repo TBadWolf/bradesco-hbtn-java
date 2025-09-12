e95a1e521236a1516bc437d4ac55475939ecc420
TBadWolf <barbara.rce@hotmail.com>
2025-09-12 16:28:00 -0300

    ajuste toString

--- Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
@@ -55,7 +55,9 @@ public class Pessoa implements Comparable<Pessoa> {
 
     @Override
     public String toString() {
-        return String.format("[%s] %s %s %s R$ %.6f", codigo, nome, cargo, idade, salario);
+        return String.format("[%d] %s %s %d R$ %s",
+                codigo, nome, cargo, idade,
+                String.format("%.6f", salario).replace('.', ','));
     }
 
     @Override
